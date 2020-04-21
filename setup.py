f9ee0b60c9bebceae99baab9809fe2a8ef386c50
susautw <susautw@gmail.com>
2020-04-21 17:26:30 +0800

    update requirements.txt and setup.py   set version to 0.4.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 requires = []
-requirement_file = Path('./requirement.txt')
+requirement_file = Path('./requirements.txt')
 assert requirement_file.is_file()
 with requirement_file.open() as fp:
     for line in fp.readlines():
@@ -15,7 +15,7 @@ print(*requires, sep=' ', end='\n')
 
 setup(
     name="imagelib",
-    version='0.3.0',
+    version='0.4.0',
     author='Rin,Su',
     license='MIT',
     packages=find_packages(), install_requires=requires
